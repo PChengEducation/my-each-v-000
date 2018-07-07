09ff19cafc7d0df8d48771c01c804807e77e878e
Pan Cheng <pchengeducation@hotmail.com>
2018-07-07 06:22:53 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
+def my_each(collection) # put argument(s) here
   # code here
+  if block_given?
+    i = 0
+
+    while i < collection.length
+      yield(collection[i])
+      i = i + 1
+    end
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
 end
